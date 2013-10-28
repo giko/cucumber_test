c6bc38df7845a3df9baa886e1d969e6acd2e1c33
nchudakov <adsenceg@ya.ru>
2013-10-28 19:10:45 +0400

    Initial

---
+++ src/main/java/cucumber_test/Calculator.java
@@ -0,0 +1,13 @@
+package cucumber_test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nchudakov
+ * Date: 28.10.13
+ * Time: 19:00
+ */
+public class Calculator {
+    public int add(int a, int b){
+        return a-b;
+    }
+}
